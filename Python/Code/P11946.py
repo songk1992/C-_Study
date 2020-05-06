46ae852e7808aaa3db430cf9fe86057869836086
SongHyunKim <zuppiy@naver.com>
2020-05-07 07:34:28 +0900

    Update P11946.py

--- Python/Code/P11946.py
+++ Python/Code/P11946.py
@@ -11,8 +11,8 @@ input = lambda : sys.stdin.readline().rstrip()
 # q score_log
 n, m, q = map(int, input().split())
 
-# [팀번호][문제번호][(301일때 못품, 301아닐때 걸린시간), 패널티시간]
-A_list = [[[301, 0] for _ in range(m+1)] for _ in range(n+1)]
+# [팀번호][문제번호][(-1일때 못품, -1아닐때 걸린시간), 패널티시간]
+A_list = [[[-1, 0] for _ in range(m+1)] for _ in range(n+1)]
 
 
 
@@ -24,7 +24,7 @@ for _ in range(q):
     question_number = int(question_number)
 
     #체크후 A_list 채워줌
-    if A_list[team_number][question_number][0] == 301:
+    if A_list[team_number][question_number][0] == -1:
         if question_marking == 'AC':
             A_list[team_number][question_number][0] = time_passed + A_list[team_number][question_number][1]
         else:
@@ -35,13 +35,14 @@ grade = [[i, 0, 0] for i in range(n+1)]
 
 for team_number in range(1, n+1):
     for question_number in range(1, m+1):
-        if A_list[team_number][question_number][0] != 301:
+        if A_list[team_number][question_number][0] != -1:
             grade[team_number][1] += 1
             grade[team_number][2] += A_list[team_number][question_number][0]
 
 # sorting
+grade = grade[1:]
 grade.sort(key=lambda x:(-x[1], x[2], x[0]))
-grade.remove([0,0,0])
+
 
 # 출력
 for x in grade:
