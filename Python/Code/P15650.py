4456372f21a241b880e3edc3e755f014be81348e
SongHyunKim <zuppiy@naver.com>
2020-05-09 01:12:42 +0900

    Update P15650.py

--- Python/Code/P15650.py
+++ Python/Code/P15650.py
@@ -1,16 +1,23 @@
 https://www.acmicpc.net/problem/15650
 
+
 from itertools import combinations
-import sys
-input = lambda : sys.stdin.readline().rstrip()
+# from itertools import combinations
+# import sys
+# input = lambda : sys.stdin.readline().rstrip()
 
-N, M = map(int, input().split())
-list_a = []
+n, m = map(int, input().split())
+# N, M = map(int, input().split())
 
-for x in range(N):
-    list_a.append((x+1))
 
-list_p = list(combinations(list_a, M))
+c = list(combinations(range(1, n+1), m))
+# list_a = []
+# for x in range(N):
+#     list_a.append((x+1))
+# list_p = list(combinations(list_a, M))
 
-for x in list_p:
+for x in c:
     print(*x)
+# for x in list_p:
+#     print(*x)
+
