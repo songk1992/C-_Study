a6b1ef85a1593f9b5632a2d23894f673acd8accd
SongHyunKim <zuppiy@naver.com>
2020-05-08 23:14:43 +0900

    Update P10974.py

--- Python/Code/P10974.py
+++ Python/Code/P10974.py
@@ -1,18 +1,28 @@
-#https://www.acmicpc.net/problem/10974
-from itertools import permutations
+#내답
 
-N = int(input())
-my_list = []
+# from itertools import permutations
+#
+# N = int(input())
+# my_list = []
+#
+# for _ in range(N):
+#     my_list.append(_+1)
+#
+# new_list = list(permutations(my_list))
+# K = len(new_list)
+#
+#
+# for i in range(K):
+#     for j in range(N):
+#         print(new_list[i][j], end=" ")
+#         if (j == N-1) and (i != K-1):
+#             print("")
 
-for _ in range(N):
-    my_list.append(_+1)
+# 출처의 답
+# https://m.blog.naver.com/wpghks7/221585604878
 
-new_list = list(permutations(my_list))
-K = len(new_list)
-
-
-for i in range(K):
-    for j in range(N):
-        print(new_list[i][j], end=" ")
-        if (j == N-1) and (i != K-1):
-            print("")
+from itertools import permutations as pm
+n = int(input())
+p = list(pm(range(1, n+1)))
+for x in p:
+    print(*x)
