5bfb5f595c258afc9432a6116b620f35cd94d776
SongHyunKim <zuppiy@naver.com>
2020-06-14 20:26:28 +0900

    부녀회장이 될테야

---
+++ Python/Code/P2775.py
@@ -0,0 +1,19 @@
+# https://www.acmicpc.net/problem/2775
+
+
+arr = [[0] * 15 for _ in range(15)]
+for _ in range(15):
+    arr[0][_] = _ + 1
+
+for i in range(1, 15):
+    for j in range(15):
+        for k in range(j+1):
+            arr[i][j] += arr[i-1][k]
+for _ in range(int(input())):
+    room_floor = int(input())
+    room_number = int(input())
+    if room_number == 0:
+        print(0)
+    else:
+        print(arr[room_floor][room_number-1])
+
